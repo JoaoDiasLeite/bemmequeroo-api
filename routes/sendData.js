fb496d53e18faccaa3086b16b752147dbcf85470
João Leite <joao.leite@wiremaze.com>
2022-02-01 21:37:53 +0000

    Final Version

--- routes/sendData.js
+++ routes/sendData.js
@@ -10,11 +10,26 @@ const gmain = process.env.GMAIL_MAIN
 
 router.get('/' , function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // handle OPTIONS method
+    if ('OPTIONS' == req.method) {
+        return res.sendStatus(200);
+    } else {
+        next();
+    }
     res.send('API is working!')
 })
 
 router.post('/',  upload.single('contactFile'),  async (req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // handle OPTIONS method
+    if ('OPTIONS' == req.method) {
+        
     console.log( req.body);
     const name = req.body.contactName;
     const email = req.body.contactEmail;
@@ -53,6 +68,10 @@ router.post('/',  upload.single('contactFile'),  async (req, res, next) =>{
               smtpTransport.close();
               return error;
           }); 
+          
+    } else {
+            next();
+        }
 })
 
 module.exports = router;
