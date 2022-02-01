ad05139cc731f2c9d9ef11c51568a02d7d1ed0b6
João Leite <joao.leite@wiremaze.com>
2022-02-01 20:02:19 +0000

    Final Version 1

--- src/app.js
+++ src/app.js
@@ -36,7 +36,62 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/.netlify/functions/api', APIRouter);
+
+var router = express.Router()
+var nodemailer = require('nodemailer');
+const FormData = require('form-data');
+const upload = require("multer")();
+
+const gpass = process.env.GMAIL_PASS;
+const greq = process.env.GMAIL_REQ;
+const gmain = process.env.GMAIL_MAIN
+
+router.get('/' , function(req, res, next){
+    res.send('API is working!')
+})
+
+router.post('/',  upload.single('contactFile'),  async (req, res, next) =>{
+    console.log( req.body);
+    const name = req.body.contactName;
+    const email = req.body.contactEmail;
+    const subject = req.body.contactSubject;
+    const message = req.body.contactMessage;
+    const anexo = req.file;
+    const smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: greq,
+        pass: gpass
+      }
+    });
+  const mail = {
+      from: greq,
+      to: gmain,
+      cc: email,
+      subject:  subject + ' || ' + name+ ' || ' + email,
+      text: message,
+      //html: "<b>Opcionalmente, pode enviar como HTML</b>"
+  } 
+  if(anexo){
+      console.log(anexo);
+      mail.attachments = [];
+      mail.attachments.push({
+          filename: anexo.originalname,
+          content: anexo.buffer
+      })
+  }
+  smtpTransport.sendMail(mail)
+          .then(response => {
+              smtpTransport.close();
+              return response;
+          })
+          .catch(error => {
+              smtpTransport.close();
+              return error;
+          }); 
+})
+
+app.use('/.netlify/functions/api', router);
 // app.use('/users', usersRouter);
 // app.use('/sendData', APIRouter)
 
@@ -57,3 +112,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports.handler = serverless(app);
+// module.exports = app;
