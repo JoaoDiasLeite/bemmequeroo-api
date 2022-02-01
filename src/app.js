a9190a610ae9689b45900f6073e85e489f9069ba
João Leite <joao.leite@wiremaze.com>
2022-02-01 19:57:22 +0000

    Final Version

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', APIRouter);
+app.use('/.netlify/functions/api', APIRouter);
 // app.use('/users', usersRouter);
 // app.use('/sendData', APIRouter)
 
